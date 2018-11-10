94f314584ebb2a549d1ffef55765f362b46bd9a6
Teo G Nistor <teo.g.nistor@gmail.com>
2018-11-10 16:10:56 +0000

    In-game UI

---
+++ Assets/Assets/Script/InGameUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class InGameUI : MonoBehaviour {
+
+    [SerializeField] Text score, timer, multiplier;
+    [SerializeField] Image progress;
+
+    private string scoreTemplate, timerTemplate, multiplierTemplate;
+    private Vector2 initialProgressSize;
+    
+	void Start () {
+        scoreTemplate = score.text;
+        timerTemplate = timer.text;
+        multiplierTemplate = multiplier.text;
+        initialProgressSize = progress.rectTransform.sizeDelta;
+	}
+
+    void LateUpdate () {
+    }
+}
